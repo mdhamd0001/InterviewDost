1a052b1b9f61e9506068b1b1a02236a2a6c024a2
Mohd Hamd <mohdhamd2003@gmail.com>
2026-02-26 11:16:40 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://interviewdost-ap3i.onrender.com"
 
 function App() {
 
